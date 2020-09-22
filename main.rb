9df37fc7fb4f038f3e0a06976ac684a5455bff27
Jonathas Tavares <jonathastavares@id.uff.br>
2020-09-22 17:38:33 -0300

    Fix Rubocop errors

--- main.rb
+++ main.rb
@@ -1,18 +1,20 @@
-def bubble_sort (array)
-    for i in 0..(array.length - 1)
-        for j in 0..(array.length - 1)
-            if array[i] < array[j]
-                store = array[i]
-                array[i] = array[j]
-                array[j] = store
-            end
-        end
-    end
-    print array
-end
+# frozen_string_literal: true
 
-i = 0
+def bubble_sort(array)
+  array.each_with_index do |x, i|
+    array.each_with_index do |y, j|
+      if x < y
+        store = array[i]
+        array[i] = array[j]
+        array[j] = store
+      end
+      next
+    end
+  end
+end
 
 bubble_sort_array = [10, 154, 8, 2, 15, 2, 1, 0]
 
 bubble_sort(bubble_sort_array)
+
+print bubble_sort_array
