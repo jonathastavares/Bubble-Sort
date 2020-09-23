99280e0995991e5d6bb4f8ecf215c46fe2e61eac
Jonathas Tavares <jonathastavares@id.uff.br>
2020-09-23 13:29:07 -0300

    Fix rubocop errors #2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 def bubble_sort(array)
   array.each_with_index do |x, i|
     array.each_with_index do |y, j|
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 def bubble_sort_by(array)
   array.each_with_index do |x, i|
     array.each_with_index do |y, j|
