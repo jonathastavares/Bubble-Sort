3b6be4b6fe2a81cc54da30a232c3fb09218a18c4
Jonathas Tavares <jonathastavares@id.uff.br>
2020-09-23 12:43:36 -0300

    FIX Rubocop errors

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -3,15 +3,14 @@
 def bubble_sort_by(array)
   array.each_with_index do |x, i|
     array.each_with_index do |y, j|
-      if yield(y, x).positive?
-        array[i], array[j]= array[j], array[i]
+      if yield(y, x).positive? { array[i], array[j] = array[j], array[i] }
       end
       next
     end
   end
 end
 
-result=bubble_sort_by(%w[hi hello hey]) do |left, right|
+result = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 
